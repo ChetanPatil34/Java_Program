ab2fde3404a43336fe7bc4fd5c7428bbe8e1c2f9
ChetanPatil34 <cp9210639@gmail.com>
2024-02-16 23:06:49 +0530

    [ADDED]:Chetan Java Code for Evenodd

---
+++ evenodd.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class evenodd
+{
+	public static void main(String args[])
+	{
+		int num;
+		Scanner obj=new Scanner(System.in);
+		System.out.print("Enter the number");
+		int a=obj.nextInt();
+
+		if(num%2==0)
+		{
+			System.out.print(n+"Even");
+		}
+		else
+		{
+			System.out.print(n+"odd");
+		}
+
+	}
+}

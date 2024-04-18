b1d7b1fdc7d9d33d5424120d74716af2fcc56efe
ChetanPatil34 <cp9210639@gmail.com>
2024-04-18 22:50:11 +0530

    [ADDED]: string length

---
+++ StringLength/src/com/chetan/stringlength/Stringlength.java
@@ -0,0 +1,14 @@
+package com.chetan.stringlength;
+
+public class Stringlength {
+
+	public static void main(String[] args) {
+
+				String var="Infogalaxy";
+				
+				System.out.println(var);
+				System.out.println(var.length()); // length() use 
+				
+			}
+
+			}

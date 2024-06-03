89b3d3bbcf54f98bba7e402c3e96cb24f691f9df
ChetanPatil34 <cp9210639@gmail.com>
2024-06-03 21:21:39 +0400

    [ADDED]:CHETAN Exception try and catch Block in Java Program

---
+++ Exception.java
@@ -0,0 +1,21 @@
+class Exception{
+
+   public static void main(String args[])
+   {
+      int a = 10;
+      int b = 0;
+
+      System.out.println("Starting Program");
+
+      try{
+		       int c = a/b;
+		       System.out.println("Division = "+c);
+	     }
+      catch(ArithmeticException e){
+
+		        System.out.println("Can not Divisible By Zero");
+	  }
+		    System.out.println("Program Ended");
+
+   }
+}

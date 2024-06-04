19460ffb76c1172ad9de0473c591be4af885ad30
ChetanPatil34 <cp9210639@gmail.com>
2024-06-04 20:37:37 +0400

    [ADDED]:CHETAN Logical Code in java

---
+++ Logical.java
@@ -0,0 +1,20 @@
+class Logical
+{
+	public static void main(String args[])
+	{
+		int i,c=0;
+		for(i=0; i<=10; i++)
+		{
+			c=i*2;
+
+			System.out.println(+c);
+			c=i*3;
+			System.out.println("\t"+c);
+			c=i*4;
+			System.out.println("\t\t"+c);
+
+
+
+		}
+	}
+}

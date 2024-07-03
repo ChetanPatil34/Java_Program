ac4ee7292fd65817f5a38a85ef21c108dc7ebbff
ChetanPatil34 <cp9210639@gmail.com>
2024-07-03 23:07:50 +0400

    [ADDED]:CHETAN Set BitManipulation code in java

---
+++ Set_BitManipulation.java
@@ -0,0 +1,13 @@
+class Set_BitManipulation{
+	public static void main(String[] args){
+
+		int n=5;
+		int pos=1;
+		int bitMask=1<<pos;
+
+		int newNumber = bitMask | n;
+		System.out.println(newNumber);
+
+
+	}
+}

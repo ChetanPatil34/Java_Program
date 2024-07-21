104c300f3f922f849298f485a499e2bb1dd8cf49
ChetanPatil34 <cp9210639@gmail.com>
2024-07-21 21:45:49 +0400

    [ADDED]:CHETAN java code for String demo

---
+++ StringDemo.java
@@ -0,0 +1,15 @@
+class StringDemo{
+
+	public static void main(String args[]){
+
+		String name1 ="Chetan";
+		String name2 = new String("Jayesh");
+		name2 ="Shubham";
+		System.out.println("Yogesh");
+
+
+		System.out.println(name1);
+		System.out.println(name2);
+		System.out.println(name2);
+		}
+	}

4f759ff68d6c1c35e2e36f591f77b291b32b2bba
ChetanPatil34 <cp9210639@gmail.com>
2024-07-25 21:42:16 +0400

    [ADDED]:CHETAN String Function Parameter in Code for java

---
+++ String_FuncParameter.java
@@ -0,0 +1,11 @@
+class String_FuncParameter{
+
+	public static void str(String name){
+
+		System.out.println("Name = "+name);
+		return;
+	}
+	public static void main(String[]args){
+		str("Chetan Patil");
+	}
+}

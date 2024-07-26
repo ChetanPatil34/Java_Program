aa1f325561b3240450750e06da27301fe619bf3c
ChetanPatil34 <cp9210639@gmail.com>
2024-07-26 21:57:08 +0400

    [ADDED]:CHETAN java Code For String Concat Function

---
+++ StringconcatFunc.java
@@ -0,0 +1,11 @@
+class StringconcatFunc
+{
+	public static void main(String args[])
+	{
+		String a = "Chetan";
+		String b = "Patil";
+
+		System.out.println("Concatination = "+a.concat(b));
+
+	}
+}

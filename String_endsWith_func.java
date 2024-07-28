9c68c26c47c2ada7a954942550a0922fd9e69ad3
ChetanPatil34 <cp9210639@gmail.com>
2024-07-28 21:14:40 +0400

    [ADDED]:CHETAN Java Code For String End With Function

---
+++ String_endsWith_func.java
@@ -0,0 +1,11 @@
+class String_endsWith_func
+{
+	public static void main(String args[]) {
+
+		String a = "Chetan Patil";
+
+		System.out.println("endsWith() = "+ a.endsWith("til"));
+		System.out.println("endsWith() = "+ a.endsWith("het"));
+		System.out.println("endsWith() = "+ a.endsWith("an"));
+	}
+}

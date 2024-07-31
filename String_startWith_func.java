c85b55fd2b7049917ff2b74fe9fea3b777c6cb6c
ChetanPatil34 <cp9210639@gmail.com>
2024-07-31 22:04:21 +0400

    [ADDED]:CHETAN Java code For String Start With Function

---
+++ String_startWith_func.java
@@ -0,0 +1,11 @@
+class String_startWith_func
+{
+	public static void main(String args[]) {
+
+		String a = "Chetan Patil";
+
+		System.out.println("startsWith() = "+ a.startsWith("Che"));
+		System.out.println("startsWith() = "+ a.startsWith("tan"));
+		System.out.println("startsWith() = "+ a.startsWith("an"));
+	}
+}

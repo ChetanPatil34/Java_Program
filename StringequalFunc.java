62958d75532c938314d4a44e3af204bc3e4479e3
ChetanPatil34 <cp9210639@gmail.com>
2024-08-05 21:11:43 +0400

    [ADDED]:CHETAN Java Code For String Equal Function

---
+++ StringequalFunc.java
@@ -0,0 +1,13 @@
+class StringequalFunc
+{
+	public static void main(String args[]) {
+
+		String a = "Chetan";
+		String b = "Arum";
+		String c = "Patil";
+
+		System.out.println("String equals = "+a.equals(b));
+		System.out.println("String not equals = "+a.equals(c));
+
+	}
+}
